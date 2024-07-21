a445fc80559bbee20933ee1c784dea1f4b5f7144
YuxuanBai1 <154863921+YuxuanBai1@users.noreply.github.com>
2024-07-21 13:01:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {  
+    event.preventDefault(); // 阻止表单的默认提交行为  
+    var username = document.getElementById('username').value;  
+    var password = document.getElementById('password').value;  
+    if (username === 'yuxuanbai' && password === 'admin123456') {  
+        // 验证成功，自动跳转到指定网页  
+        window.location.href = 'index.html'; 
+    } else {   
+        alert('用户名或密码错误！');  
+    }  
+});
